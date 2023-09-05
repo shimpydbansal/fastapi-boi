bc274057bff29c80ec31332dc0466032515b7735
Shimpy Bansal <shimpy@example.com>
2023-09-05 10:23:19 +0530

    Fix psql connection issue

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -e
 set -x
 
-# Activate the virtual environment
-source .venv/bin/activate
-
 # Set (or export) necessary environment variables, if they aren't set elsewhere
 # Example:
 # export POSTGRES_SCHEMA=your_schema
@@ -15,7 +12,10 @@ echo "CREATE SCHEMA IF NOT EXISTS $POSTGRES_SCHEMA;" > /tmp/init.sql
 echo "ALTER USER $POSTGRES_USER SET search_path TO $POSTGRES_SCHEMA;" >> /tmp/init.sql
 
 # Run the script with psql to create the schema and set the search path
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /tmp/init.sql
+psql -h localhost -p 5432 -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /tmp/init.sql
+
+# Activate the virtual environment
+source .venv/bin/activate
 
 # Run migrations
 alembic upgrade head
