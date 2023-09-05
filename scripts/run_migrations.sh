507728f34889c6d84768735971483331f24d4a35
Shimpy Bansal <shimpy@example.com>
2023-09-05 06:57:36 +0530

    setup postgress and create the schema before running migrations

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -4,5 +4,18 @@ set -x
 # Activate the virtual environment
 source .venv/bin/activate
 
+# Set (or export) necessary environment variables, if they aren't set elsewhere
+# Example:
+# export POSTGRES_SCHEMA=your_schema
+# export POSTGRES_USER=your_user
+# export POSTGRES_DB=your_database
+
+# Create the SQL script using the POSTGRES_SCHEMA environment variable
+echo "CREATE SCHEMA IF NOT EXISTS $POSTGRES_SCHEMA;" > /tmp/init.sql
+echo "ALTER USER $POSTGRES_USER SET search_path TO $POSTGRES_SCHEMA;" >> /tmp/init.sql
+
+# Run the script with psql to create the schema and set the search path
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /tmp/init.sql
+
 # Run migrations
 alembic upgrade head
