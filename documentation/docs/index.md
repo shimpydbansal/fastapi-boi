bffe139f7c4fbf58d33ac6f7dd5658911d23a3ca
Shimpy Bansal <shimpy@example.com>
2023-09-06 09:03:00 +0530

    fix: Documentation Broken links

--- documentation/docs/index.md
+++ documentation/docs/index.md
@@ -3,10 +3,9 @@ sphinx-quickstart on Fri Jul 21 21:09:55 2023.
 You can adapt this file completely to your liking, but it should at least
 contain the root `toctree` directive. -->
 
-# Welcome to Chatbot’s documentation!
+# Welcome to FastAPI Boilerplate documentation documentation!
 
 # Indices and tables
 
-* [Index](genindex.md)
-* [Module Index](py-modindex.md)
-* [Search Page](search.md)
+* [Index](index.md)
+* [Intro](intro.md)
---
+++ scripts/bump_version.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+set -x
+
+# Get the last commit message
+commit_message=$(git log -1 --pretty=%B)
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Determine the version bump based on the commit message
+if echo "$commit_message" | grep -q "^BREAKING CHANGE:"; then
+    bump2version major
+elif echo "$commit_message" | grep -q "^feature:"; then
+    bump2version minor
+elif echo "$commit_message" | grep -q "^fix:"; then
+    bump2version patch
+else
+    echo "No version bump due to commit message format"
+    exit 0  # If you want to fail the build in this case, replace with exit 1
+fi
